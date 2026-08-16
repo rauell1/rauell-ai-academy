0c62cab558c84463092f4c6ef7b47424f355ae3c
Roy Otieno | Energy & Mobility Systems <royokola3@gmail.com>
2026-08-16 10:42:43 +0300

    fix: add explicit API rewrites in vercel.json and universal auth handler

--- api/auth/[...all].ts
+++ api/auth/[...all].ts
@@ -1,3 +1,9 @@
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "../../src/server/auth";
-export default toNodeHandler(auth);
+
+export default async function handler(req: any, res?: any) {
+  if (!res || (typeof req.json === "function" && !req.headers?.host)) {
+    return auth.handler(req);
+  }
+  return toNodeHandler(auth)(req, res);
+}
